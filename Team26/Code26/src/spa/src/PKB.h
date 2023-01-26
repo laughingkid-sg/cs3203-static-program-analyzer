a6c3f387a23676930b521610cd5a4063af7dbd0d
Zheng Teck <adamgoh1998@gmail.com>
2023-01-26 17:00:04 +0800

    fix: fix linting

--- Team26/Code26/src/spa/src/PKB.h
+++ Team26/Code26/src/spa/src/PKB.h
@@ -1,21 +1,19 @@
 #pragma once
 
-#include<stdio.h>
+#include <stdio.h>
 #include <iostream>
 #include <string>
 #include <vector>
 
-using namespace std;
-typedef short PROC;
+typedef std::int64_t PROC;
 
 class TNode;
 
 class VarTable;  // no need to #include "VarTable.h" as all I need is pointer
 
 class PKB {
-public:
-	static VarTable* varTable; 
-	static int setProcToAST(PROC p, TNode* r);
-	static TNode* getRootAST (PROC p);
-
+ public:
+    static VarTable* varTable;
+    static int setProcToAST(PROC p, TNode* r);
+    static TNode* getRootAST(PROC p);
 };
