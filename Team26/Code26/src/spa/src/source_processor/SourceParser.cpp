662a5d456f1c3d9fdbb433de630db34089a4c1b0
ovidharshini <ovidharshini@gmail.com>
2023-02-02 19:58:05 +0800

    feat: add minimal source parsing

--- Team26/Code26/src/spa/src/source_processor/SourceParser.cpp
+++ Team26/Code26/src/spa/src/source_processor/SourceParser.cpp
@@ -1,1 +1,94 @@
 #include "SourceParser.h"
+
+#define INITIAL_STMT_INDEX 1
+#define PROCEDURE_KEYWORD "procedure"
+#define STMTLIST_START "{"
+#define STMTLIST_END "}"
+#define CALL_KEYWORD "call"
+#define PRINT_KEYWORD "print"
+#define READ_KEYWORD "read"
+#define STMT_END ";"
+
+SourceParser::SourceParser(std::vector<std::shared_ptr<Token>> tokens)
+    : Parser(tokens), stmtIndex(INITIAL_STMT_INDEX) {}
+
+std::shared_ptr<ProgramNode> SourceParser::parse() {
+    std::vector<std::shared_ptr<ProcedureNode>> procedureList;
+    while (!isTypeOf(TokenType::TOKEN_END_OF_FILE)) {
+        procedureList.emplace_back(parseProcedure());
+    }
+
+    if (procedureList.empty()) {
+        // TODO(oviya): Throw exception
+    }
+
+    return std::make_shared<ProgramNode>(procedureList);
+}
+
+std::shared_ptr<ProcedureNode> SourceParser::parseProcedure() {
+    parseNext(PROCEDURE_KEYWORD);
+    std::shared_ptr<Token> nameToken = parseNext(TokenType::TOKEN_NAME);
+    parseNext(STMTLIST_START);
+    std::shared_ptr<StmtListNode> stmtListNode = parseStmtList();
+    parseNext(STMTLIST_END);
+
+    return std::make_shared<ProcedureNode>(nameToken->getValue(), stmtListNode);
+}
+
+std::shared_ptr<StmtListNode> SourceParser::parseStmtList() {
+    std::vector<std::shared_ptr<StmtNode>> stmtList;
+
+    while (!isValueOf(STMTLIST_END)) {
+        if (!isTypeOf(TokenType::TOKEN_NAME)) {
+            // TODO(oviya): Throw exception
+        }
+
+        std::shared_ptr<StmtNode> stmtNode;
+        std::shared_ptr<Token> nameToken = parseNext(TokenType::TOKEN_NAME);
+
+        if (nameToken->getValue() == CALL_KEYWORD) {
+            stmtNode = parseCall();
+        }
+        else if (nameToken->getValue() == PRINT_KEYWORD) {
+            stmtNode = parsePrint();
+        }
+        else if (nameToken->getValue() == READ_KEYWORD) {
+            stmtNode = parseRead();
+        }
+        else {
+            // TODO(oviya): Throw exception
+        }
+
+        stmtList.emplace_back(stmtNode);
+    }
+
+    if (stmtList.empty()) {
+        // TODO(oviya): Throw exception
+    }
+
+    return std::make_shared<StmtListNode>(stmtList);
+}
+
+std::shared_ptr<CallNode> SourceParser::parseCall() {
+    std::shared_ptr<Token> nameToken = parseNext(TokenType::TOKEN_NAME);
+    parseNext(STMT_END);
+    stmtIndex++;
+
+    return std::make_shared<CallNode>(stmtIndex, nameToken->getValue());
+}
+
+std::shared_ptr<PrintNode> SourceParser::parsePrint() {
+    std::shared_ptr<Token> nameToken = parseNext(TokenType::TOKEN_NAME);
+    parseNext(STMT_END);
+    stmtIndex++;
+
+    return std::make_shared<PrintNode>(stmtIndex, nameToken->getValue());
+}
+
+std::shared_ptr<ReadNode> SourceParser::parseRead() {
+    std::shared_ptr<Token> nameToken = parseNext(TokenType::TOKEN_NAME);
+    parseNext(STMT_END);
+    stmtIndex++;
+
+    return std::make_shared<ReadNode>(stmtIndex, nameToken->getValue());
+}
--- Team26/Code26/src/spa/src/source_processor/SourceParser.h
+++ Team26/Code26/src/spa/src/source_processor/SourceParser.h
@@ -1,4 +1,27 @@
 #pragma once
 
-class SourceParser {
+#include <vector>
+#include "common/Parser.h"
+#include "common/tokens/Token.h"
+#include "node/ProgramNode.h"
+#include "node/ProcedureNode.h"
+#include "node/StmtListNode.h"
+#include "node/statement_node/ReadNode.h"
+#include "node/statement_node/PrintNode.h"
+#include "node/statement_node/CallNode.h"
+
+class SourceParser : public Parser {
+private:
+    typedef Parser Super;
+    int stmtIndex;
+    std::shared_ptr<ProcedureNode> parseProcedure();
+    std::shared_ptr<StmtListNode> parseStmtList();
+    std::shared_ptr<CallNode> parseCall();
+    std::shared_ptr<PrintNode> parsePrint();
+    std::shared_ptr<ReadNode> parseRead();
+
+public:
+    SourceParser(std::vector<std::shared_ptr<Token>> tokens);
+    std::shared_ptr<ProgramNode> parse();
+
 };
