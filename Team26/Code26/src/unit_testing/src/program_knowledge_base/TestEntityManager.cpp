39d6f263b16fc0fdfd37e3a3941852af9b1b47f6
wei-yutong <weiyutongggg@gmail.com>
2023-02-05 21:16:37 +0800

    test: add tests for EntityManager and RelationshipManager

---
+++ Team26/Code26/src/unit_testing/src/program_knowledge_base/TestEntityManager.cpp
@@ -0,0 +1,72 @@
+#include "catch.hpp"
+#include "program_knowledge_base/entity/EntityManager.h"
+
+TEST_CASE("EntityManager insertEntity int") {
+    EntityManager<int> entityManager;
+
+    // reject repeated entry
+    REQUIRE(entityManager.insertEntity(3));
+    REQUIRE_FALSE(entityManager.insertEntity(3));
+
+    // insert new
+    REQUIRE(entityManager.insertEntity(4));
+}
+
+TEST_CASE("EntityManager insertEntity string") {
+    EntityManager<std::string> entityManager;
+
+    // reject repeated entry
+    REQUIRE(entityManager.insertEntity("x"));
+    REQUIRE_FALSE(entityManager.insertEntity("x"));
+
+    // insert new
+    REQUIRE(entityManager.insertEntity("flag"));
+}
+
+TEST_CASE("EntityManager isEmpty int") {
+    EntityManager<int> entityManager;
+    REQUIRE(entityManager.isEmpty());
+    REQUIRE(entityManager.insertEntity(3));
+    REQUIRE_FALSE(entityManager.isEmpty());
+}
+
+TEST_CASE("EntityManager isEmpty string") {
+    EntityManager<std::string> entityManager;
+    REQUIRE(entityManager.isEmpty());
+    REQUIRE(entityManager.insertEntity("x"));
+    REQUIRE_FALSE(entityManager.isEmpty());
+}
+
+TEST_CASE("EntityManager contains int") {
+    EntityManager<int> entityManager;
+    REQUIRE_FALSE(entityManager.contains(3));
+    REQUIRE(entityManager.insertEntity(3));
+    REQUIRE(entityManager.contains(3));
+}
+
+TEST_CASE("EntityManager contains string") {
+    EntityManager<std::string> entityManager;
+    REQUIRE_FALSE(entityManager.contains("x"));
+    REQUIRE(entityManager.insertEntity("x"));
+    REQUIRE(entityManager.contains("x"));
+}
+
+TEST_CASE("EntityManager getAllEntitiesEntries int") {
+    EntityManager<int> entityManager;
+    std::unordered_set<int> stmtNo_set = {3, 11};
+    std::unordered_set<int> empty_set = {};
+    REQUIRE(entityManager.insertEntity(3));
+    REQUIRE(entityManager.insertEntity(11));
+    REQUIRE(entityManager.getAllEntitiesEntries() == stmtNo_set);
+    REQUIRE_FALSE(entityManager.getAllEntitiesEntries() == empty_set);
+}
+
+TEST_CASE("EntityManager getAllEntitiesEntries string") {
+    EntityManager<std::string> entityManager;
+    std::unordered_set<std::string> varName_set = {"x", "radius"};
+    std::unordered_set<std::string> empty_set = {};
+    REQUIRE(entityManager.insertEntity("x"));
+    REQUIRE(entityManager.insertEntity("radius"));
+    REQUIRE(entityManager.getAllEntitiesEntries() == varName_set);
+    REQUIRE_FALSE(entityManager.getAllEntitiesEntries() == empty_set);
+}
