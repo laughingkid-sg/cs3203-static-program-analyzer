7af870911be73f8c447b429b96b0791a7ca89f6c
shaelynl <shaelyn.lamxy@gmail.com>
2023-02-06 13:55:58 +0800

    Fix bug in Query Validator

--- Team26/Code26/src/spa/src/query_processing_system/QueryValidator.h
+++ Team26/Code26/src/spa/src/query_processing_system/QueryValidator.h
@@ -4,6 +4,9 @@
 #include "query_processing_system/exception/QueryException.h"
 #include "query_processing_system/exception/QueryExceptionMessages.h"
 
+#include <algorithm>
+#include <unordered_set>
+
 class QueryValidator {
  private:
     // Validation for Declarations
@@ -28,6 +31,9 @@ class QueryValidator {
      * @param query Query that is passed.
      */
     void validateSynonymInSelectClauseWasDeclared(Query&);
+
+    bool containsSelectClauseSynonymInDeclaration(const std::unordered_set<std::string>& declarationSynonyms,
+                                      const std::unordered_set<std::string>& selectClauseSynonyms);
 
 
  public:
