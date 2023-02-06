ef754a2309dd72b2ba76f5c33d071efe6912bca9
Zheng Teck <adamgoh1998@gmail.com>
2023-02-06 21:46:06 +0800

    fix: add vector initialization

--- Team26/Code26/src/spa/src/source_processor/design_extractor/extractor/RelationshipExtractor.h
+++ Team26/Code26/src/spa/src/source_processor/design_extractor/extractor/RelationshipExtractor.h
@@ -1,10 +1,11 @@
 #pragma once
 #include <memory>
+#include <vector>
 #include "source_processor/design_extractor/extractor/AbstractSyntaxTreeExtractor.h"
 
 class RelationshipExtractor : public AbstractSyntaxTreeExtractor {
  private:
-    std::shared_ptr<std::vector<int>> simpleFollow;
+    std::shared_ptr<std::vector<int>> simpleFollow = std::make_shared<std::vector<int>>();
  public:
     explicit RelationshipExtractor(std::shared_ptr<WriteOnlyStorage> storage);
     void extractProcedure(std::shared_ptr<ProcedureNode> node) override;
