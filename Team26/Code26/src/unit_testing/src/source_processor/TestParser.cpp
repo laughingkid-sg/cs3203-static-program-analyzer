9a119d5bd7e53d53d559925ea858f88ddb32d87f
ovidharshini <ovidharshini@gmail.com>
2023-02-09 14:40:10 +0800

    test: source processor read parsing

--- Team26/Code26/src/unit_testing/src/source_processor/TestParser.cpp
+++ Team26/Code26/src/unit_testing/src/source_processor/TestParser.cpp
@@ -10,6 +10,7 @@ TEST_CASE("Parser parse empty") {
 }
 
 TEST_CASE("Parser parse procedure") {
+    // one procedure: positive test case
     std::vector<std::shared_ptr<Token>> tokens;
     tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "procedure"));
     tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "proc"));
@@ -23,6 +24,8 @@ TEST_CASE("Parser parse procedure") {
     CHECK(programNode->procedureList[0]->stmtListNode->stmtList.size() == 0);
 
     tokens.clear();
+
+    // two procedures: positive test case
     tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "procedure"));
     tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "proc1"));
     tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "{"));
@@ -39,4 +42,25 @@ TEST_CASE("Parser parse procedure") {
     CHECK(programNode->procedureList[0]->stmtListNode->stmtList.size() == 0);
     CHECK(programNode->procedureList[1]->procedureName == "proc2");
     CHECK(programNode->procedureList[1]->stmtListNode->stmtList.size() == 0);
+}
+
+TEST_CASE("Parser parse read") {
+    // one read stmt: positive test case
+    std::vector<std::shared_ptr<Token>> tokens;
+    tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "procedure"));
+    tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "proc"));
+    tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "{"));
+    tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "read"));
+    tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "x"));
+    tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, ";"));
+    tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "}"));
+    tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_END_OF_FILE, ""));
+    auto programNode = Parser(tokens).parse();
+
+    REQUIRE(programNode->procedureList.size() == 1);
+    CHECK(programNode->procedureList[0]->procedureName == "proc");
+    REQUIRE(programNode->procedureList[0]->stmtListNode->stmtList.size() == 1);
+    CHECK(programNode->procedureList[0]->stmtListNode->stmtList[0]->stmtIndex == 1);
+    // TODO(oviya): compare type of derived class
+    //REQUIRE(typeid(programNode->procedureList[0]->stmtListNode->stmtList[0]) == typeid(ReadNode));
 }
