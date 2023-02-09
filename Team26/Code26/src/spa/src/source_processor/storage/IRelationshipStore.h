b3a572344370e29b1ae88b8a590e376f2efbb25c
Zheng Teck <adamgoh1998@gmail.com>
2023-02-10 02:27:52 +0800

    chore: implement storage functions

--- Team26/Code26/src/spa/src/source_processor/storage/IRelationshipStore.h
+++ Team26/Code26/src/spa/src/source_processor/storage/IRelationshipStore.h
@@ -1,9 +1,13 @@
 #pragma once
 
+#include <string>
+
 class IRelationshipStore {
  public:
     virtual void insertFollowsRelationship(int &first_param, int &second_param) = 0;
     virtual void insertParentsRelationship(int &first_param, int &second_param) = 0;
-    virtual void insertUsesSRelationship(int &first_param, int &second_param) = 0;
-    virtual void insertModifiesSRelationship(int &first_param, int &second_param) = 0;
+    virtual void insertUsesSRelationship(int &first_param, std::string &second_param) = 0;
+    virtual void insertModifiesSRelationship(int &first_param, std::string &second_param) = 0;
+    virtual void insertUsesPRelationship(std::string  &first_param, std::string &second_param) = 0;
+    virtual void insertModifiesPRelationship(std::string  &first_param, std::string &second_param) = 0;
 };
--- Team26/Code26/src/spa/src/source_processor/storage/RelationshipStore.cpp
+++ Team26/Code26/src/spa/src/source_processor/storage/RelationshipStore.cpp
@@ -10,11 +10,23 @@ void RelationshipStore::insertFollowsRelationship(int &first_param, int &second_
 }
 
 void RelationshipStore::insertParentsRelationship(int &first_param, int &second_param) {
+    relationshipStorage->getParentManager()->insertRelationship(first_param, second_param,
+                                                                relationshipStorage->getParentTManager());
 }
 
-void RelationshipStore::insertUsesSRelationship(int &first_param, int &second_param) {
+void RelationshipStore::insertUsesSRelationship(int &first_param, std::string &second_param) {
+    relationshipStorage->getUsesSManager()->insertRelationship(first_param, second_param);
 }
 
-void RelationshipStore::insertModifiesSRelationship(int &first_param, int &second_param) {
+void RelationshipStore::insertModifiesSRelationship(int &first_param, std::string &second_param) {
+    relationshipStorage->getModifiesSManager()->insertRelationship(first_param, second_param);
+}
+
+void RelationshipStore::insertUsesPRelationship(std::string &first_param, std::string &second_param) {
+    relationshipStorage->getUsesPManager()->insertRelationship(first_param, second_param);
+}
+
+void RelationshipStore::insertModifiesPRelationship(std::string &first_param, std::string &second_param) {
+    relationshipStorage->getModifiesPManager()->insertRelationship(first_param, second_param);
 }
 
