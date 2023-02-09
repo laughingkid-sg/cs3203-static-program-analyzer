26d0fd9d1360733aa05bdcc1aabd017fae6c283f
ovidharshini <ovidharshini@gmail.com>
2023-02-09 15:06:20 +0800

    test: source processor print and call parsing

--- Team26/Code26/src/unit_testing/src/source_processor/TestParser.cpp
+++ Team26/Code26/src/unit_testing/src/source_processor/TestParser.cpp
@@ -66,3 +66,49 @@ TEST_CASE("Parser parse read") {
     REQUIRE_NOTHROW(readNode = dynamic_cast<ReadNode*>(programNode->procedureList[0]->stmtListNode->stmtList[0].get()));
     CHECK(readNode->varName == "xyz");
 }
+
+TEST_CASE("Parser parse print") {
+    // one read stmt: positive test case
+    std::vector<std::shared_ptr<Token>> tokens;
+    tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "procedure"));
+    tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "proc"));
+    tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "{"));
+    tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "print"));
+    tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "xyz"));
+    tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, ";"));
+    tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "}"));
+    tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_END_OF_FILE, ""));
+    auto programNode = Parser(tokens).parse();
+
+    REQUIRE(programNode->procedureList.size() == 1);
+    CHECK(programNode->procedureList[0]->procedureName == "proc");
+    REQUIRE(programNode->procedureList[0]->stmtListNode->stmtList.size() == 1);
+    CHECK(programNode->procedureList[0]->stmtListNode->stmtList[0]->stmtIndex == 1);
+    REQUIRE(programNode->procedureList[0]->stmtListNode->stmtList[0]->stmtType == StmtType::STMT_PRINT);
+    PrintNode* printNode;
+    REQUIRE_NOTHROW(printNode = dynamic_cast<PrintNode*>(programNode->procedureList[0]->stmtListNode->stmtList[0].get()));
+    CHECK(printNode->varName == "xyz");
+}
+
+TEST_CASE("Parser parse call") {
+    // one read stmt: positive test case
+    std::vector<std::shared_ptr<Token>> tokens;
+    tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "procedure"));
+    tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "proc"));
+    tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "{"));
+    tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "call"));
+    tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "xyz"));
+    tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, ";"));
+    tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "}"));
+    tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_END_OF_FILE, ""));
+    auto programNode = Parser(tokens).parse();
+
+    REQUIRE(programNode->procedureList.size() == 1);
+    CHECK(programNode->procedureList[0]->procedureName == "proc");
+    REQUIRE(programNode->procedureList[0]->stmtListNode->stmtList.size() == 1);
+    CHECK(programNode->procedureList[0]->stmtListNode->stmtList[0]->stmtIndex == 1);
+    REQUIRE(programNode->procedureList[0]->stmtListNode->stmtList[0]->stmtType == StmtType::STMT_CALL);
+    CallNode* callNode;
+    REQUIRE_NOTHROW(callNode = dynamic_cast<CallNode*>(programNode->procedureList[0]->stmtListNode->stmtList[0].get()));
+    CHECK(callNode->processName == "xyz");
+}
