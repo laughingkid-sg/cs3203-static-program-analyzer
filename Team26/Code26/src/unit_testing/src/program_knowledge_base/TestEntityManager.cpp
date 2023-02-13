bf9313f0eef0d0d0daacbc484dc9ce93cd503520
Chng Zi Hao <chngzihao@gmail.com>
2023-02-14 03:05:39 +0800

    test: add additional checks for getAllEntitiesEntries

--- Team26/Code26/src/unit_testing/src/program_knowledge_base/TestEntityManager.cpp
+++ Team26/Code26/src/unit_testing/src/program_knowledge_base/TestEntityManager.cpp
@@ -53,20 +53,28 @@ TEST_CASE("EntityManager contains string") {
 
 TEST_CASE("EntityManager getAllEntitiesEntries int") {
     EntityManager<int> entityManager;
+    auto set = entityManager.getAllEntitiesEntries();
+    REQUIRE(set.size() == 0);
     std::unordered_set<int> stmtNo_set = {3, 11};
     std::unordered_set<int> empty_set = {};
     REQUIRE(entityManager.insertEntity(3));
     REQUIRE(entityManager.insertEntity(11));
+    set = entityManager.getAllEntitiesEntries();
+    REQUIRE(set.size() == 2);
     REQUIRE(entityManager.getAllEntitiesEntries() == stmtNo_set);
     REQUIRE_FALSE(entityManager.getAllEntitiesEntries() == empty_set);
 }
 
 TEST_CASE("EntityManager getAllEntitiesEntries string") {
     EntityManager<std::string> entityManager;
+    auto set = entityManager.getAllEntitiesEntries();
+    REQUIRE(set.size() == 0);
     std::unordered_set<std::string> varName_set = {"x", "radius"};
     std::unordered_set<std::string> empty_set = {};
     REQUIRE(entityManager.insertEntity("x"));
     REQUIRE(entityManager.insertEntity("radius"));
+    set = entityManager.getAllEntitiesEntries();
+    REQUIRE(set.size() == 2);
     REQUIRE(entityManager.getAllEntitiesEntries() == varName_set);
     REQUIRE_FALSE(entityManager.getAllEntitiesEntries() == empty_set);
 }
