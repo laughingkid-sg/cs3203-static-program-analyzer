314eb39bb17347659db6c8e2fe6dfefb2891b9fd
Zheng Teck <adamgoh1998@gmail.com>
2023-02-13 21:27:23 +0800

    refactor: use base class for declaration

--- Team26/Code26/src/spa/src/source_processor/design_extractor/DesignExtractor.h
+++ Team26/Code26/src/spa/src/source_processor/design_extractor/DesignExtractor.h
@@ -2,18 +2,17 @@
 
 #include <memory>
 #include "source_processor/node/ProgramNode.h"
-#include "program_knowledge_base/WriteOnlyStorage.h"
-#include "source_processor/design_extractor/extractor/EntityExtractor.h"
-#include "source_processor/design_extractor/extractor/RelationshipExtractor.h"
 #include "source_processor/storage/Store.h"
-#include "source_processor/design_extractor/extractor/PatternExtractor.h"
+#include "program_knowledge_base/WriteOnlyStorage.h"
+#include "source_processor/design_extractor/extractor/BaseExtractor.h"
+
 
 class DesignExtractor {
  private:
     std::shared_ptr<IStore> store;
-    std::unique_ptr<EntityExtractor> entityExtractor;
-    std::unique_ptr<RelationshipExtractor> relationshipExtractor;
-    std::unique_ptr<PatternExtractor> patternExtractor;
+    std::unique_ptr<BaseExtractor> entityExtractor;
+    std::unique_ptr<BaseExtractor> relationshipExtractor;
+    std::unique_ptr<BaseExtractor> patternExtractor;
 
  public:
     explicit DesignExtractor(const std::shared_ptr<IStore>& store);
