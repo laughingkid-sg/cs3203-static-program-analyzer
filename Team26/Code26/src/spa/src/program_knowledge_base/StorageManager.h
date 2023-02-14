5df7c916d0c405a93739997db93b99a8e305ad7e
Chng Zi Hao <chngzihao@gmail.com>
2023-02-15 03:37:28 +0800

    refactor: storage manager

--- Team26/Code26/src/spa/src/program_knowledge_base/StorageManager.h
+++ Team26/Code26/src/spa/src/program_knowledge_base/StorageManager.h
@@ -3,22 +3,29 @@
 #include <vector>
 #include <memory>
 #include "Storage.h"
-#include "program_knowledge_base/relationship/WriteOnlyRelationshipManager.h"
-#include "program_knowledge_base/entity/WriteOnlyEntityManager.h"
-#include "program_knowledge_base/pattern/WriteOnlyPatternManager.h"
-#include "program_knowledge_base/relationship/ReadOnlyRelationshipManager.h"
-#include "program_knowledge_base/entity/ReadOnlyEntityManager.h"
-#include "program_knowledge_base/pattern/ReadOnlyPatternManager.h"
+#include "program_knowledge_base/relationship/IWriteRelationshipManager.h"
+#include "program_knowledge_base/entity/IWriteEntityManager.h"
+#include "program_knowledge_base/pattern/IWritePatternManager.h"
+#include "program_knowledge_base/relationship/IReadRelationshipManager.h"
+#include "program_knowledge_base/entity/IReadEntityManager.h"
+#include "program_knowledge_base/pattern/IReadPatternManager.h"
 
-using WriteStorage = Storage<WriteOnlyRelationshipManger, WriteOnlyEntityManager, WriteOnlyPatternManager>;
-using ReadStroage =  Storage<ReadOnlyRelationshipManager,
-        ReadOnlyEntityManager, ReadOnlyPatternManager>;
+using WriteStorage = Storage<IWriteRelationshipManager, IWriteEntityManager, IWritePatternManager>;
+using ReadStroage =  Storage<IReadRelationshipManager, IReadEntityManager, IReadPatternManager>;
 
 class StorageManager {
  private:
     std::shared_ptr<StorageUtil> storageUtil;
  public:
-    StorageManager();
-    std::shared_ptr<WriteStorage> getWriteManager();
-    std::shared_ptr<ReadStroage> getReadManager();
+    StorageManager() {
+        storageUtil = std::make_shared<StorageUtil>();
+    }
+
+    std::shared_ptr<WriteStorage> getWriteManager() {
+        return std::make_shared<WriteStorage>(storageUtil);
+    }
+
+    std::shared_ptr<ReadStroage> getReadManager() {
+        return std::make_shared<ReadStroage>(storageUtil);
+    }
 };
