6c867f3275c4081d378e0396191dad9011ebb2eb
Haozeee <teyhaoze@gmail.com>
2023-02-16 18:01:12 +0800

    test: Result table unit tests

--- Team26/Code26/src/unit_testing/src/query_processing_system/evaluator/TestResultTable.cpp
+++ Team26/Code26/src/unit_testing/src/query_processing_system/evaluator/TestResultTable.cpp
@@ -6,19 +6,93 @@ std::unordered_set<std::string> tableAItems {"a", "b", "c"};
 std::string tableACol = "col1";
 auto tableA = ResultTable::createSingleColumnTable(tableACol, tableAItems);
 
-// Create double columns to be used for testing
-std::unordered_set<std::string> tableBItems1 {"a", "b", "c", "d"};
-std::unordered_set<std::string> tableBItems2 {"a", "b", "c", "e"};
+// Create double columns table to be used for testing
+std::unordered_set<std::string> tableBItems1 {"a", "b"};
+std::unordered_set<std::string> tableBItems2 {"c", "d"};
 std::string tableBCol1 = "col1";
 std::string tableBCol2 = "col2";
 auto tableB = ResultTable::createDoubleColumnTable(tableBCol1, tableBItems1, tableBCol2, tableBItems2);
 
-TEST_CASE("Single Table") {
+// Create double columns table to be used for testing
+std::unordered_set<std::string> tableCItems1 {"b"};
+std::unordered_set<std::string> tableCItems2 {"c", "d"};
+auto tableC = ResultTable::createDoubleColumnTable(tableBCol1, tableCItems1, tableBCol2, tableCItems2);
+
+TEST_CASE("Double Table") {
     // Check correct dimensions of table
-    REQUIRE(tableA->getNumberOfRows() == tableAItems.size());
-    // REQUIRE(tableA->getColumnsNames() == {tableACol});
-    std::unordered_set<std::string> items {"a", "b", "c"};
-    std::string colName = "col1";
-    auto singleColTable = ResultTable::createSingleColumnTable(colName, items);
+    auto tableBColNames = tableB->getColumnsNames();
+    REQUIRE(tableBColNames.size() == 2);
+    REQUIRE(tableBColNames[0] == tableBCol1);
+    REQUIRE(tableBColNames[1] == tableBCol2);
+    REQUIRE(tableB->getNumberOfRows() == (tableBItems1.size() * tableBItems2.size()));
+
+    // Check values stored at the correct position
+    // Check if column values are correct
+    auto col1Values = tableB->getColumnValues(tableBCol1);
+    for (auto i : col1Values) {
+        REQUIRE(tableBItems1.count(i));
+    }
+    // Check if row values are correct
+    auto row1Values = tableB->getRow(0);
+    std::vector<std::string> expectedValues {"a", "c"};
+    REQUIRE(row1Values == expectedValues);
+
+    // Check column naming
+    REQUIRE(tableB->getColumnNumber(tableBCol1) == 0);
+    REQUIRE(tableB->getColumnNumber(tableBCol2) == 1);
+
+    // Test matching column function
+    std::vector<std::string> res = {tableBCol1};
+    REQUIRE(tableB->hasMatchingColumns({tableBCol1, "TEST"}) == res);
+    res = {};
+    REQUIRE(tableB->hasMatchingColumns({"TEST"}) == res);
+
+    // Test Insert row
+    std::vector<std::string> rowToInsert = {"g", "h"};
+    tableB->insertRow(rowToInsert);
+    REQUIRE(tableB->getNumberOfRows() == (tableBItems1.size() * tableBItems2.size() + 1));
+    REQUIRE(tableB->getRow(tableB->getNumberOfRows() - 1) == rowToInsert);
 }
 
+TEST_CASE("Join One Matching Columns") {
+    // Join table A and B
+    auto res = ResultTable::joinOnColumns(tableA, tableB, {tableBCol1});
+    // Check result table column names
+    std::vector<std::string> colNames = {tableBCol1, tableBCol2};
+    REQUIRE(res->getColumnsNames() == colNames);
+    // Check Number of row
+    REQUIRE(res->getNumberOfRows() == 4);
+    std::vector<std::string> row1 = {"a", "c"};
+    std::vector<std::string> row2 = {"a", "d"};
+    std::vector<std::string> row3 = {"b", "c"};
+    std::vector<std::string> row4 = {"b", "d"};
+    REQUIRE(res->getRow(0) == row1);
+    REQUIRE(res->getRow(1) == row2);
+    REQUIRE(res->getRow(2) == row3);
+    REQUIRE(res->getRow(3) == row4);
+}
+
+TEST_CASE("Join two matching column") {
+    // Join table B and C
+    auto res = ResultTable::joinOnColumns(tableB, tableC, {tableBCol1, tableBCol2});
+    // Check result table column names
+    std::vector<std::string> colNames = {tableBCol1, tableBCol2};
+    REQUIRE(res->getColumnsNames() == colNames);
+    // Check Number of row
+    REQUIRE(res->getNumberOfRows() == 2);
+    std::vector<std::string> row1 = {"b", "c"};
+    std::vector<std::string> row2 = {"b", "d"};
+    REQUIRE(res->getRow(0) == row1);
+    REQUIRE(res->getRow(1) == row2);
+}
+
+TEST_CASE("Joining With Empty Table") {
+    auto emptyTable = std::make_shared<ResultTable>(tableBCol1);
+    // Join tableC and empty table
+    auto res = ResultTable::joinOnColumns(tableC, emptyTable, {tableBCol1});
+    // Check result table column names
+    std::vector<std::string> colNames = {tableBCol1, tableBCol2};
+    REQUIRE(res->getColumnsNames() == colNames);
+    // Check Number of row
+    REQUIRE(res->getNumberOfRows() == 0);
+}
