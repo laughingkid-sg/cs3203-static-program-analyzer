b248962c7c495534ba50282f2f75abd71ad74065
Haozeee <teyhaoze@gmail.com>
2023-02-16 20:42:32 +0800

    style: fix style

--- Team26/Code26/src/spa/src/query_processing_system/evaluator/clause_evaluator/such_that_clause_evaluator/IntStringClauseEvaluator.h
+++ Team26/Code26/src/spa/src/query_processing_system/evaluator/clause_evaluator/such_that_clause_evaluator/IntStringClauseEvaluator.h
@@ -2,6 +2,7 @@
 #include "SuchThatClauseEvaluator.h"
 #include <string>
 #include <unordered_set>
+#include <unordered_map>
 #include <memory>
 
 class IntStringClauseEvaluator : public SuchThatClauseEvaluator<int, std::string> {
