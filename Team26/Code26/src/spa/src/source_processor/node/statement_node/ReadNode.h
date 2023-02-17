bdfd20f2fb37e42d3605d8f4c73a018e3b70e087
Zheng Teck <adamgoh1998@gmail.com>
2023-02-17 11:35:12 +0800

    refactor: apply DRY

--- Team26/Code26/src/spa/src/source_processor/node/statement_node/ReadNode.h
+++ Team26/Code26/src/spa/src/source_processor/node/statement_node/ReadNode.h
@@ -6,7 +6,6 @@
 
 class ReadNode : public VariableNameNode, public std::enable_shared_from_this<ReadNode> {
  public:
-
     ReadNode(int stmtIndex, std::string varName);
 
     void evaluate(IStmtExtractor& extractor) override;
--- Team26/Code26/src/spa/src/source_processor/node/statement_node/VariableNameNode.cpp
+++ Team26/Code26/src/spa/src/source_processor/node/statement_node/VariableNameNode.cpp
@@ -1,8 +1,6 @@
 #include "VariableNameNode.h"
-
 #include <utility>
 
 VariableNameNode::VariableNameNode(int stmtIndex, std::string varName, StmtType stmtType) :
     StmtNode(stmtIndex, stmtType), varName(std::move(varName))  {
-
 }
