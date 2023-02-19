457a2a4e8f1b2b7e8534435040f092c857dbec4b
Zheng Teck <adamgoh1998@gmail.com>
2023-02-19 12:59:41 +0800

    fix: add error handling for shunting yard parser

--- Team26/Code26/src/spa/src/common/exception/ExceptionMessage.cpp
+++ Team26/Code26/src/spa/src/common/exception/ExceptionMessage.cpp
@@ -5,3 +5,8 @@ extern const char TokenizerLeadingZeroExceptionMessage[] = "Leading zeros detect
 extern const char ParserMismatchTokenTypeExceptionMessage[] = "Mismatch token type detected in parser.";
 extern const char ParserMismatchTokenValueExceptionMessage[] = "Mismatch token type detected in parser. Token should "
                                                                "not be value of [";
+
+extern const char ParserShuntingYardParserUnknownOperatorExceptionMessage[] =
+        R"lit(Invalid operator detected. Only these operators are supported "+", "-", "*", "/", "%", "(" , ").")lit";
+extern const char ParserShuntingYardParserInvalidExpressionExceptionMessage[] = "Invalid expression detected.";
+
