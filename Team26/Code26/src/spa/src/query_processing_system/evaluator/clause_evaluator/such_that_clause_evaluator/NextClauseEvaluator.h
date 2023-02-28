26af272cd801e6a554bbcd63cdb7603d9b88c3c5
Haozeee <teyhaoze@gmail.com>
2023-02-28 20:16:49 +0800

    style: fix linting

--- Team26/Code26/src/spa/src/query_processing_system/evaluator/clause_evaluator/such_that_clause_evaluator/NextClauseEvaluator.h
+++ Team26/Code26/src/spa/src/query_processing_system/evaluator/clause_evaluator/such_that_clause_evaluator/NextClauseEvaluator.h
@@ -5,7 +5,7 @@
 #include <unordered_set>
 
 class NextClauseEvaluator : public IntIntClauseEvaluator {
-public:
+ public:
     NextClauseEvaluator(Argument left, Argument right);
 
     std::unordered_map<int, std::unordered_set<int>> getRelationshipManager(StoragePointer storage) override;
