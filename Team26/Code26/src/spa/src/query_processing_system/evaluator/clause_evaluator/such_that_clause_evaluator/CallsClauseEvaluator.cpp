51b1dc83513bbf00525c93bba663d1366122f8a5
Zheng Teck <adamgoh1998@gmail.com>
2023-03-05 23:56:48 +0800

    fix: merge conflict

--- Team26/Code26/src/spa/src/query_processing_system/evaluator/clause_evaluator/such_that_clause_evaluator/CallsClauseEvaluator.cpp
+++ Team26/Code26/src/spa/src/query_processing_system/evaluator/clause_evaluator/such_that_clause_evaluator/CallsClauseEvaluator.cpp
@@ -5,12 +5,12 @@ CallsClauseEvaluator::CallsClauseEvaluator(Argument left, Argument right)
 
 std::unordered_map<std::string, std::unordered_set<std::string>>
 CallsClauseEvaluator::getRelationshipManager(StoragePointer storage) {
-    return storage->getCallsManager()->getAllRelationshipEntries();
+    return storage->getCallPManager()->getAllRelationshipEntries();
 }
 
 std::unordered_map<std::string, std::unordered_set<std::string>>
 CallsClauseEvaluator::getOppositeRelationshipManager(StoragePointer storage) {
-    return storage->getCallsManager()->getAllReversedRelationshipEntries();
+    return storage->getCallPManager()->getAllReversedRelationshipEntries();
 }
 
 void CallsClauseEvaluator::handleRightWildcard() {
