e4c33f144b7a9debf470eebb48148f26e506f4d4
Haozeee <teyhaoze@gmail.com>
2023-03-07 23:41:16 +0800

    fix: windows build error

--- Team26/Code26/src/spa/src/query_processing_system/evaluator/Util.cpp
+++ Team26/Code26/src/spa/src/query_processing_system/evaluator/Util.cpp
@@ -9,7 +9,7 @@ std::unordered_set<std::string> Util::intSetToStringSet(std::unordered_set<int> 
 
 
 std::unordered_map<std::string, std::unordered_set<std::string>>
-Util::intMapTostringMap(std::unordered_map<int, std::unordered_set<int>> &intMap) {
+Util::intMapTostringMap(std::unordered_map<int, std::unordered_set<int>> intMap) {
     std::unordered_map<std::string, std::unordered_set<std::string>> res;
     for (auto const& [k, v] : intMap) {
         res.insert({std::to_string(k), intSetToStringSet(v)});
@@ -18,7 +18,7 @@ Util::intMapTostringMap(std::unordered_map<int, std::unordered_set<int>> &intMap
 }
 
 std::unordered_map<std::string, std::unordered_set<std::string>>
-Util::intStringMapTostringMap(std::unordered_map<int, std::unordered_set<std::string>> &intMap) {
+Util::intStringMapTostringMap(std::unordered_map<int, std::unordered_set<std::string>> intMap) {
     std::unordered_map<std::string, std::unordered_set<std::string>> res;
     for (auto const& [k, v] : intMap) {
         res.insert({std::to_string(k), v});
