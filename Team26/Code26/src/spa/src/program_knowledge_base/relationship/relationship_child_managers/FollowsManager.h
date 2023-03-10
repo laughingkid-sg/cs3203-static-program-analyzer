c5e282313d95866bbf4dfd5933d694ed36dbaabc
Zheng Teck <adamgoh1998@gmail.com>
2023-03-10 10:35:16 +0800

    update: transitive managers

--- Team26/Code26/src/spa/src/program_knowledge_base/relationship/relationship_child_managers/FollowsManager.h
+++ Team26/Code26/src/spa/src/program_knowledge_base/relationship/relationship_child_managers/FollowsManager.h
@@ -3,12 +3,4 @@
 #include "program_knowledge_base/relationship/RelationshipManager.h"
 
 class FollowsManager : public RelationshipManager<int, int> {
- public:
-    bool insertRelationship(int first_param, int second_param,
-                            std::shared_ptr<IWriteRelationshipManager<int, int>> followsTManager) override {
-        bool flag = RelationshipManager::insertRelationship(first_param, second_param);
-        // duplicate insert into FollowsTManager
-        bool flag1 = followsTManager->insertRelationship(first_param, second_param);
-        return flag && flag1;
-    }
 };
