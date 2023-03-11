2761f05afacb30f58231fb4245d19a5deb83cdfd
Zheng Teck <adamgoh1998@gmail.com>
2023-03-11 14:40:14 +0800

    refactor: remove pattern extractor references

--- Team26/Code26/src/spa/src/source_processor/node/statement_node/IfNode.cpp
+++ Team26/Code26/src/spa/src/source_processor/node/statement_node/IfNode.cpp
@@ -2,7 +2,6 @@
 
 #include <utility>
 #include "source_processor/design_extractor/interface/statement_extractor/IStmtExtractor.h"
-#include "source_processor/design_extractor/interface/IPatternExtractor.h"
 
 IfNode::IfNode(int stmtIndex, std::shared_ptr<CondExprNode> condExprNode,
     std::shared_ptr<StmtListNode> thenStmtListNode, std::shared_ptr<StmtListNode> elseStmtListNode)
@@ -12,9 +11,5 @@ IfNode::IfNode(int stmtIndex, std::shared_ptr<CondExprNode> condExprNode,
     elseStmtListNode(std::move(elseStmtListNode)) {}
 
 void IfNode::evaluate(IStmtExtractor& extractor) {
-    extractor.extractIf(this->shared_from_this());
-}
-
-void IfNode::evaluatePattern(IPatternExtractor &extractor) {
     extractor.extractIf(this->shared_from_this());
 }
--- Team26/Code26/src/spa/src/source_processor/node/statement_node/StmtNode.cpp
+++ Team26/Code26/src/spa/src/source_processor/node/statement_node/StmtNode.cpp
@@ -1,7 +1,3 @@
 #include "StmtNode.h"
 
 StmtNode::StmtNode(int stmtIndex, StmtType stmtType) : stmtIndex(stmtIndex), stmtType(stmtType) {}
-
-void StmtNode::evaluatePattern(IPatternExtractor &extractor) {
-    throw new SourceExtractorException(BaseExtractorNotSupportedEvaluationExceptionMessage);
-}
