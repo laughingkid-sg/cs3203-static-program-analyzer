a9a828ad51bae5dc636ada7ee91cdd969972bf62
Zheng Teck <adamgoh1998@gmail.com>
2023-03-18 12:27:40 +0800

    refactor: call test cases

--- Team26/Code26/src/unit_testing/src/source_processor/parser/TestParseCall.cpp
+++ Team26/Code26/src/unit_testing/src/source_processor/parser/TestParseCall.cpp
@@ -56,7 +56,7 @@ TEST_CASE("Parser Call") {
         std::shared_ptr<ProgramNode> programNode = parser.getProgramNode();
 
         REQUIRE(programNode->procedureList.size() == 1);
-        REQUIRE(programNode->procedureList[0]->stmtListNode->stmtList.size() >= 1);
+        REQUIRE(!programNode->procedureList[0]->stmtListNode->stmtList.empty());
 
         std::shared_ptr<StmtNode> stmtNode = programNode->procedureList[0]->stmtListNode->stmtList[0];
         CHECK(stmtNode->stmtIndex == 1);
@@ -134,7 +134,7 @@ TEST_CASE("Parser Call") {
         REQUIRE(programNode->procedureList.size() == 2);
 
         SECTION("first Procedure") {
-            REQUIRE(programNode->procedureList[0]->stmtListNode->stmtList.size() >= 1);
+            REQUIRE(!programNode->procedureList[0]->stmtListNode->stmtList.empty());
             std::shared_ptr<StmtNode> stmtNode = programNode->procedureList[0]->stmtListNode->stmtList[0];
             CHECK(stmtNode->stmtIndex == 1);
             REQUIRE(stmtNode->stmtType == StmtType::STMT_CALL);
@@ -145,7 +145,7 @@ TEST_CASE("Parser Call") {
         }
 
         SECTION("second Procedure") {
-            REQUIRE(programNode->procedureList[1]->stmtListNode->stmtList.size() >= 1);
+            REQUIRE(!programNode->procedureList[1]->stmtListNode->stmtList.empty());
             std::shared_ptr<StmtNode> stmtNode = programNode->procedureList[1]->stmtListNode->stmtList[0];
             CHECK(stmtNode->stmtIndex == 2);
             REQUIRE(stmtNode->stmtType == StmtType::STMT_CALL);
