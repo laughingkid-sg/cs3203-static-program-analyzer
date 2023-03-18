ef78da1dec300e17e3dd2d0dbd6505566bd6fef3
Zheng Teck <adamgoh1998@gmail.com>
2023-03-18 12:50:37 +0800

    refactor: assign test cases

--- Team26/Code26/src/unit_testing/src/source_processor/parser/TestParseAssign.cpp
+++ Team26/Code26/src/unit_testing/src/source_processor/parser/TestParseAssign.cpp
@@ -6,170 +6,170 @@
 TEST_CASE("Parser Assign") {
 std::vector<std::shared_ptr<Token>> tokens;
 
-SECTION("single Assign: missing operator") {
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "procedure"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "proc"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "{"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "x"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "1"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, ";"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "}"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_END_OF_FILE, ""));
-REQUIRE_THROWS(Parser(tokens).parse());
-}
+    SECTION("single Assign: missing operator") {
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "procedure"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "proc"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "{"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "x"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "1"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, ";"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "}"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_END_OF_FILE, ""));
+        REQUIRE_THROWS(Parser(tokens).parse());
+    }
 
-SECTION("single Assign: missing variable") {
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "procedure"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "proc"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "{"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "="));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "1"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, ";"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "}"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_END_OF_FILE, ""));
-REQUIRE_THROWS(Parser(tokens).parse());
-}
+    SECTION("single Assign: missing variable") {
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "procedure"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "proc"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "{"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "="));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "1"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, ";"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "}"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_END_OF_FILE, ""));
+        REQUIRE_THROWS(Parser(tokens).parse());
+    }
 
-SECTION("single Assign: missing expr") {
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "procedure"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "proc"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "{"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "x"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "="));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, ";"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "}"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_END_OF_FILE, ""));
-REQUIRE_THROWS(Parser(tokens).parse());
-}
+    SECTION("single Assign: missing expr") {
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "procedure"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "proc"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "{"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "x"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "="));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, ";"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "}"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_END_OF_FILE, ""));
+        REQUIRE_THROWS(Parser(tokens).parse());
+    }
 
-SECTION("single Assign: missing semicolon") {
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "procedure"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "proc"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "{"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "x"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "="));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "1"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "}"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_END_OF_FILE, ""));
-REQUIRE_THROWS(Parser(tokens).parse());
-}
+    SECTION("single Assign: missing semicolon") {
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "procedure"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "proc"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "{"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "x"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "="));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "1"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "}"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_END_OF_FILE, ""));
+        REQUIRE_THROWS(Parser(tokens).parse());
+    }
 
-SECTION("single Assign factor_int") {
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "procedure"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "proc"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "{"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "read"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "="));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_INTEGER, "1"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, ";"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "}"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_END_OF_FILE, ""));
-REQUIRE_NOTHROW(Parser(tokens).parse());
+    SECTION("single Assign factor_int") {
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "procedure"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "proc"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "{"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "read"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "="));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_INTEGER, "1"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, ";"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "}"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_END_OF_FILE, ""));
+        REQUIRE_NOTHROW(Parser(tokens).parse());
 
-auto parser = Parser(tokens);
-parser.parse();
-std::shared_ptr<ProgramNode> programNode = parser.getProgramNode();
+        auto parser = Parser(tokens);
+        parser.parse();
+        std::shared_ptr<ProgramNode> programNode = parser.getProgramNode();
 
-REQUIRE(programNode->procedureList.size() == 1);
-REQUIRE(!programNode->procedureList[0]->stmtListNode->stmtList.empty());
+        REQUIRE(programNode->procedureList.size() == 1);
+        REQUIRE(!programNode->procedureList[0]->stmtListNode->stmtList.empty());
 
-std::shared_ptr<StmtNode> stmtNode = programNode->procedureList[0]->stmtListNode->stmtList[0];
-CHECK(stmtNode->stmtIndex == 1);
-REQUIRE(stmtNode->stmtType == StmtType::STMT_ASSIGN);
+        std::shared_ptr<StmtNode> stmtNode = programNode->procedureList[0]->stmtListNode->stmtList[0];
+        CHECK(stmtNode->stmtIndex == 1);
+        REQUIRE(stmtNode->stmtType == StmtType::STMT_ASSIGN);
 
-AssignNode* assignNode;
-REQUIRE_NOTHROW(assignNode = dynamic_cast<AssignNode*>(stmtNode.get()));
-CHECK(assignNode->varName == "read");
+        AssignNode* assignNode;
+        REQUIRE_NOTHROW(assignNode = dynamic_cast<AssignNode*>(stmtNode.get()));
+        CHECK(assignNode->varName == "read");
 
-REQUIRE(assignNode->exprVariables.empty());
-REQUIRE(assignNode->exprConstants.size() == 1);
-REQUIRE(assignNode->exprConstants.find(1) != assignNode->exprConstants.end());
-}
+        REQUIRE(assignNode->exprVariables.empty());
+        REQUIRE(assignNode->exprConstants.size() == 1);
+        REQUIRE(assignNode->exprConstants.find(1) != assignNode->exprConstants.end());
+    }
 
-SECTION("single Assign factor_var") {
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "procedure"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "proc"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "{"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "x"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "="));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "y"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, ";"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "}"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_END_OF_FILE, ""));
-REQUIRE_NOTHROW(Parser(tokens).parse());
+    SECTION("single Assign factor_var") {
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "procedure"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "proc"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "{"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "x"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "="));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "y"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, ";"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "}"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_END_OF_FILE, ""));
+        REQUIRE_NOTHROW(Parser(tokens).parse());
 
-auto parser = Parser(tokens);
-parser.parse();
-std::shared_ptr<ProgramNode> programNode = parser.getProgramNode();
+        auto parser = Parser(tokens);
+        parser.parse();
+        std::shared_ptr<ProgramNode> programNode = parser.getProgramNode();
 
-REQUIRE(programNode->procedureList.size() == 1);
-REQUIRE(programNode->procedureList[0]->stmtListNode->stmtList.size() >= 1);
+        REQUIRE(programNode->procedureList.size() == 1);
+        REQUIRE(!programNode->procedureList[0]->stmtListNode->stmtList.empty());
 
-std::shared_ptr<StmtNode> stmtNode = programNode->procedureList[0]->stmtListNode->stmtList[0];
-CHECK(stmtNode->stmtIndex == 1);
-REQUIRE(stmtNode->stmtType == StmtType::STMT_ASSIGN);
+        std::shared_ptr<StmtNode> stmtNode = programNode->procedureList[0]->stmtListNode->stmtList[0];
+        CHECK(stmtNode->stmtIndex == 1);
+        REQUIRE(stmtNode->stmtType == StmtType::STMT_ASSIGN);
 
-AssignNode* assignNode;
-REQUIRE_NOTHROW(assignNode = dynamic_cast<AssignNode*>(stmtNode.get()));
-CHECK(assignNode->varName == "x");
+        AssignNode* assignNode;
+        REQUIRE_NOTHROW(assignNode = dynamic_cast<AssignNode*>(stmtNode.get()));
+        CHECK(assignNode->varName == "x");
 
-REQUIRE(assignNode->exprConstants.empty());
-REQUIRE(!assignNode->exprVariables.empty());
-REQUIRE(assignNode->exprVariables.find("y") != assignNode->exprVariables.end());
-}
+        REQUIRE(assignNode->exprConstants.empty());
+        REQUIRE(!assignNode->exprVariables.empty());
+        REQUIRE(assignNode->exprVariables.find("y") != assignNode->exprVariables.end());
+    }
 
-SECTION("double Assign same Procedure") {
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "procedure"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "proc"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "{"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "x"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "="));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_INTEGER, "1"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, ";"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "y"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "="));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "x"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, ";"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "}"));
-tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_END_OF_FILE, ""));
-REQUIRE_NOTHROW(Parser(tokens).parse());
+    SECTION("double Assign same Procedure") {
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "procedure"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "proc"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "{"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "x"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "="));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_INTEGER, "1"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, ";"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "y"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "="));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "x"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, ";"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_SPECIAL_CHAR, "}"));
+        tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_END_OF_FILE, ""));
+        REQUIRE_NOTHROW(Parser(tokens).parse());
 
-auto parser = Parser(tokens);
-parser.parse();
-std::shared_ptr<ProgramNode> programNode = parser.getProgramNode();
+        auto parser = Parser(tokens);
+        parser.parse();
+        std::shared_ptr<ProgramNode> programNode = parser.getProgramNode();
 
-REQUIRE(programNode->procedureList.size() == 1);
-REQUIRE(programNode->procedureList[0]->stmtListNode->stmtList.size() >= 2);
+        REQUIRE(programNode->procedureList.size() == 1);
+        REQUIRE(programNode->procedureList[0]->stmtListNode->stmtList.size() >= 2);
 
-SECTION("first Assign") {
-std::shared_ptr<StmtNode> stmtNode = programNode->procedureList[0]->stmtListNode->stmtList[0];
-CHECK(stmtNode->stmtIndex == 1);
-REQUIRE(stmtNode->stmtType == StmtType::STMT_ASSIGN);
+        SECTION("first Assign") {
+        std::shared_ptr<StmtNode> stmtNode = programNode->procedureList[0]->stmtListNode->stmtList[0];
+        CHECK(stmtNode->stmtIndex == 1);
+        REQUIRE(stmtNode->stmtType == StmtType::STMT_ASSIGN);
 
-AssignNode *assignNode;
-REQUIRE_NOTHROW(assignNode = dynamic_cast<AssignNode *>(stmtNode.get()));
-CHECK(assignNode->varName == "x");
+        AssignNode *assignNode;
+        REQUIRE_NOTHROW(assignNode = dynamic_cast<AssignNode *>(stmtNode.get()));
+        CHECK(assignNode->varName == "x");
 
-REQUIRE(!assignNode->exprConstants.empty());
-REQUIRE(assignNode->exprVariables.empty());
-REQUIRE(assignNode->exprConstants.find(1) != assignNode->exprConstants.end());
-}
+        REQUIRE(!assignNode->exprConstants.empty());
+        REQUIRE(assignNode->exprVariables.empty());
+        REQUIRE(assignNode->exprConstants.find(1) != assignNode->exprConstants.end());
+    }
 
 
-SECTION("second Assign") {
-std::shared_ptr<StmtNode> stmtNode = programNode->procedureList[0]->stmtListNode->stmtList[1];
-CHECK(stmtNode->stmtIndex == 2);
-REQUIRE(stmtNode->stmtType == StmtType::STMT_ASSIGN);
+        SECTION("second Assign") {
+            std::shared_ptr<StmtNode> stmtNode = programNode->procedureList[0]->stmtListNode->stmtList[1];
+            CHECK(stmtNode->stmtIndex == 2);
+            REQUIRE(stmtNode->stmtType == StmtType::STMT_ASSIGN);
 
-AssignNode* assignNode;
-REQUIRE_NOTHROW(assignNode = dynamic_cast<AssignNode*>(stmtNode.get()));
-CHECK(assignNode->varName == "y");
+            AssignNode* assignNode;
+            REQUIRE_NOTHROW(assignNode = dynamic_cast<AssignNode*>(stmtNode.get()));
+            CHECK(assignNode->varName == "y");
 
-REQUIRE(assignNode->exprConstants.empty());
-REQUIRE(!assignNode->exprVariables.empty());
-REQUIRE(assignNode->exprVariables.find("x") != assignNode->exprVariables.end());
-}
-}
+            REQUIRE(assignNode->exprConstants.empty());
+            REQUIRE(!assignNode->exprVariables.empty());
+            REQUIRE(assignNode->exprVariables.find("x") != assignNode->exprVariables.end());
+        }
+    }
 
     SECTION("double Assign different Procedure") {
         tokens.push_back(std::make_shared<Token>(TokenType::TOKEN_NAME, "procedure"));
@@ -198,7 +198,7 @@ REQUIRE(assignNode->exprVariables.find("x") != assignNode->exprVariables.end());
         REQUIRE(programNode->procedureList.size() == 2);
 
         SECTION("first Procedure") {
-            REQUIRE(programNode->procedureList[0]->stmtListNode->stmtList.size() >= 1);
+            REQUIRE(!programNode->procedureList[0]->stmtListNode->stmtList.empty());
             std::shared_ptr<StmtNode> stmtNode = programNode->procedureList[0]->stmtListNode->stmtList[0];
             CHECK(stmtNode->stmtIndex == 1);
             REQUIRE(stmtNode->stmtType == StmtType::STMT_ASSIGN);
@@ -212,7 +212,7 @@ REQUIRE(assignNode->exprVariables.find("x") != assignNode->exprVariables.end());
         }
 
         SECTION("second Procedure") {
-            REQUIRE(programNode->procedureList[1]->stmtListNode->stmtList.size() >= 1);
+            REQUIRE(!programNode->procedureList[1]->stmtListNode->stmtList.empty());
             std::shared_ptr<StmtNode> stmtNode = programNode->procedureList[1]->stmtListNode->stmtList[0];
             CHECK(stmtNode->stmtIndex == 2);
             REQUIRE(stmtNode->stmtType == StmtType::STMT_ASSIGN);
