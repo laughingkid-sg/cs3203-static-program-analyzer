ac719197b60dfec2feb2839f3eb552a7af725e4a
Zheng Teck <adamgoh1998@gmail.com>
2023-03-26 19:18:42 +0800

    test: add entity extractor call test

--- Team26/Code26/src/unit_testing/src/source_processor/design_extractor/TestEntityExtractor.cpp
+++ Team26/Code26/src/unit_testing/src/source_processor/design_extractor/TestEntityExtractor.cpp
@@ -77,4 +77,52 @@ TEST_CASE("Test Entity Extractor") {
         REQUIRE(!entityStore->variableSet.begin()->find(variable));
         REQUIRE(mockPatternStore->assignStore.size() == 2);
     }
+
+    SECTION("Calls Entity Extraction") {
+        std::shared_ptr<MockEntityStore> entityStore = std::make_shared<MockEntityStore>();
+        std::shared_ptr<MockPatternStore> patternStore = std::make_shared<MockPatternStore>();
+
+        std::unique_ptr<EntityExtractor> entityExtractor = std::make_unique<EntityExtractor>
+                (entityStore, patternStore);
+        /**
+         * procedure a { call callNode1; } // line 1
+         * procedure callNode1 { print print }; // line 2
+         * **/
+        int nodeIndex = 0;
+        const std::string  firstProcedureName = "a";
+        const std::string callNodeName = "callNode1";
+        const std::string printNodeName = "print";
+        std::shared_ptr<CallNode> callNode = std::make_shared<CallNode>(nodeIndex++, callNodeName);
+        std::shared_ptr<PrintNode> printNode = std::make_shared<PrintNode>(nodeIndex++, printNodeName);
+
+        std::vector<std::shared_ptr<StmtNode>> stmtList1;
+        stmtList1.emplace_back(callNode);
+        std::shared_ptr<StmtListNode> stmtListNode1 = std::make_shared<StmtListNode>(stmtList1);
+
+        std::vector<std::shared_ptr<StmtNode>> stmtList2;
+        stmtList2.emplace_back(printNode);
+        std::shared_ptr<StmtListNode> stmtListNode2 = std::make_shared<StmtListNode>(stmtList2);
+
+        std::shared_ptr<ProcedureNode> procedureNode1 =
+                std::make_shared<ProcedureNode>(firstProcedureName, stmtListNode1);
+        std::shared_ptr<ProcedureNode> procedureNode2 =
+                std::make_shared<ProcedureNode>(callNodeName, stmtListNode2);
+
+        std::vector<std::shared_ptr<ProcedureNode>> procedureList;
+        procedureList.emplace_back(procedureNode1);
+        procedureList.emplace_back(procedureNode2);
+
+        std::shared_ptr<ProgramNode> programNode = std::make_shared<ProgramNode>(procedureList);
+
+        entityExtractor->extractProgram(programNode);
+
+        REQUIRE(entityStore->callSet.size() == 1);
+        REQUIRE(entityStore->printSet.size() == 1);
+        REQUIRE(entityStore->procedureSet.size() == 2);
+
+        REQUIRE(entityStore->callSet.find(callNode) != entityStore->callSet.end());
+        REQUIRE(entityStore->printSet.find(printNode) != entityStore->printSet.end());
+        REQUIRE(entityStore->procedureSet.find(procedureNode1) != entityStore->procedureSet.end());
+        REQUIRE(entityStore->procedureSet.find(procedureNode2) != entityStore->procedureSet.end());
+    }
 }
