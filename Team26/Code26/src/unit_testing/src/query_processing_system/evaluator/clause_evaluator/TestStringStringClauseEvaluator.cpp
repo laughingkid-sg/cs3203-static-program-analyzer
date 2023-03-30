535017f09c29565ae11a1955e7ec6aee38b926ab
wei-yutong <weiyutongggg@gmail.com>
2023-03-30 22:45:52 +0800

    fix: amend incorrect argument types

--- Team26/Code26/src/unit_testing/src/query_processing_system/evaluator/clause_evaluator/TestStringStringClauseEvaluator.cpp
+++ Team26/Code26/src/unit_testing/src/query_processing_system/evaluator/clause_evaluator/TestStringStringClauseEvaluator.cpp
@@ -99,25 +99,6 @@ TEST_CASE("Test Wildcard Synonym - string, string") {
 
     // Set up
     Argument leftArg = Argument(ArgumentType::WILDCARD, "_", DesignEntity::NONE);
-    Argument rightArg = Argument(ArgumentType::WILDCARD, "v", DesignEntity::VARIABLE);
-    auto testEvaluator = MockStringStringClauseEvaluator(leftArg, rightArg);
-
-    // Evaluation Results
-    std::shared_ptr<ResultTable> res;
-
-    // Has results
-    testEvaluator.evaluateClause(testStorage);
-    res = testEvaluator.getClauseResult();
-    std::unordered_set<std::string> expectedResultsVar {};
-    REQUIRE(res->getColumnValues("v") == expectedResultsVar);
-}
-
-TEST_CASE("Test Synonym String - string, string") {
-    // Tests relation with parameter (p, v)
-    std::shared_ptr<MockStorageReader> testStorage = std::make_shared<MockStorageReader>();
-
-    // Set up
-    Argument leftArg = Argument(ArgumentType::SYNONYM, "p", DesignEntity::PROCEDURE);
     Argument rightArg = Argument(ArgumentType::SYNONYM, "v", DesignEntity::VARIABLE);
     auto testEvaluator = MockStringStringClauseEvaluator(leftArg, rightArg);
 
@@ -127,8 +108,32 @@ TEST_CASE("Test Synonym String - string, string") {
     // Has results
     testEvaluator.evaluateClause(testStorage);
     res = testEvaluator.getClauseResult();
-    std::unordered_set<std::string> expectedResultsProc {"a", "b", "d"};
     std::unordered_set<std::string> expectedResultsVar {"b", "c", "d", "e"};
-    REQUIRE(res->getColumnValues("p") == expectedResultsProc);
     REQUIRE(res->getColumnValues("v") == expectedResultsVar);
+}
+
+TEST_CASE("Test Synonym String - string, string") {
+    // Tests relation with parameter (p, "c")
+    std::shared_ptr<MockStorageReader> testStorage = std::make_shared<MockStorageReader>();
+
+    // Set up
+    Argument leftArg = Argument(ArgumentType::SYNONYM, "p", DesignEntity::PROCEDURE);
+    Argument rightArg = Argument(ArgumentType::CHARACTERSTRING, "c", DesignEntity::NONE);
+    auto testEvaluator = MockStringStringClauseEvaluator(leftArg, rightArg);
+
+    // Evaluation Results
+    std::shared_ptr<ResultTable> res;
+
+    // Has results
+    testEvaluator.evaluateClause(testStorage);
+    res = testEvaluator.getClauseResult();
+    std::unordered_set<std::string> expectedResultsProc {"a", "b"};
+    REQUIRE(res->getColumnValues("p") == expectedResultsProc);
+
+    // Has no results
+    leftArg = Argument(ArgumentType::CHARACTERSTRING, "fe", DesignEntity::NONE);
+    testEvaluator = MockStringStringClauseEvaluator(leftArg, rightArg);
+    testEvaluator.evaluateClause(testStorage);
+    res = testEvaluator.getClauseResult();
+    REQUIRE(res->hasNoResults());
 }
